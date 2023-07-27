e678db41a8c1df4d93e1102658b025372bb381a9
philip13 <cuevas_castillo@hotmail.com>
2023-07-27 10:07:00 -0600

    Update address

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -146,7 +146,7 @@ export function Footer({description}) {
                 <path
                   d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
               </svg>
-              New York, NY 10012, US
+              Guadalajara, Jalisco
             </p>
             <p className="mb-4 flex items-center justify-center md:justify-start">
               <svg
